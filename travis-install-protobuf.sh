0b9974197a852d9333357a5886d6750a61c9d4c3
Ryan Deak <rdeak@eharmony.com>
2015-07-16 12:19:48 -0700

    added cd - to script

--- travis-install-protobuf.sh
+++ travis-install-protobuf.sh
@@ -5,6 +5,7 @@ if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
   tar -xzvf protobuf-2.4.1.tar.gz
   cd protobuf-2.4.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  cd -
 else
   echo "Using cached directory."
 fi
